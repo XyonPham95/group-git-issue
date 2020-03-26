a09cf565a00a7350c6f87961fc3756b41f716829
XyonPham <mr.gau95@gmail.com>
2020-03-26 18:43:25 +0700

    add branch

---
+++ src/components/ViewIssue.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ViewIssue() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/components/ViewRepo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ViewRepo() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
